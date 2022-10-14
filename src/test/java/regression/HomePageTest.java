433bcf13dd76eb6009a03c28b9fdf336ef9c7a59
bheemesh <manu.bheemesh@gmail.com>
2022-10-15 00:28:21 +0530

    initial commit

---
+++ src/test/java/regression/HomePageTest.java
@@ -0,0 +1,11 @@
+package regression;
+
+import org.testng.annotations.Test;
+
+public class HomePageTest {
+
+    @Test
+    public void homePageTitleValidation() {
+        System.out.println("hey i am testing Home Page title");
+    }
+}
---
+++ src/test/java/regression/LoginPageTest.java
@@ -0,0 +1,18 @@
+package regression;
+
+import org.testng.annotations.Test;
+
+public class LoginPageTest {
+
+    @Test
+    public void loginPageTitleValidation() {
+        System.out.println("hey i am testing page title");
+    }
+
+    @Test
+    public void loginPageSoureValidation() {
+        System.out.println("hey i am testing page source validation");
+    }
+
+
+}
