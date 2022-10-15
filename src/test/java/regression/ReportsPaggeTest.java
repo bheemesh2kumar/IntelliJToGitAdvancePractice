2ac833aadf0875b16cefa25494305b53d7d45793
sirimadhavi <115865078+sirimadhavi@users.noreply.github.com>
2022-10-15 13:01:45 +0530

    Create ReportsPaggeTest.java
    this is commit of reports page added

---
+++ src/test/java/regression/ReportsPaggeTest.java
@@ -0,0 +1,18 @@
+package regression;
+
+import org.testng.annotations.Test;
+
+public class ReportsPaggeTest {
+
+    @Test
+    public void loginPageTitleValidation() {
+        System.out.println("hey i am report page page title");
+    }
+
+    @Test
+    public void loginPageSoureValidation() {
+        System.out.println("hey i am report page source validation");
+    }
+
+
+}
